88e928b4d0facf62ee3487773d452b3378b0a435
Gagik Papoyan <papoyan.qa@gmail.com>
2016-08-13 11:27:04 +0400

    UI fix

---
+++ EffectList.swift
@@ -0,0 +1,18 @@
+//
+//  EffectList.swift
+//  PolaPic
+//
+//  Created by Gagik Papoyan on 8/13/16.
+//  Copyright © 2016 Gagik Papoyan. All rights reserved.
+//
+
+import UIKit
+
+class EffectList: UICollectionViewCell {
+
+    @IBOutlet weak var EffectPreview: UIImageView!
+    @IBOutlet weak var EffectName: UILabel!
+
+
+    
+}
